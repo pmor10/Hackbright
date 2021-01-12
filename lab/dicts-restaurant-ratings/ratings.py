9be56bd97f165a0b06cb2a0d8ab9d324b47fee0f
pmor10 <paresamorton@gmail.com>
2021-01-11 16:54:30 -0800

    completed ratings.py
    Co-Authored By: Rachel Perkins <rachel.elysia.perkins@gmail.com>

--- lab/dicts-restaurant-ratings/ratings.py
+++ lab/dicts-restaurant-ratings/ratings.py
@@ -1,39 +1,49 @@
 """Restaurant rating lister."""
 
-
-# put your code here
-#make a function
 def ratings_dictionary(filename):
-    """
+    """Input a text file of restaurant ratings to return dictionary of ratings."""
 
-    """
-
-    # get the file
     data = open(filename)
 
-    # create an empty dict.
     restaurant_ratings = {}
     
-    # loop through the file lines
+    #Iterate through lines in file and adds key and value to dictionary
     for line in data:
-        # each line as a string of resturant name split from rating 
         line = line.rstrip().split(":")
-        # add the resturant name as the key and rating as the value to the dict.
         restaurant_ratings[line[0]] = int(line[1])
 
     return restaurant_ratings
 
-#create a new function that alphabetizes the keys into a list
 def sorted_ratings(filename):
-    """
-    docstring
-    """
+    """Input a text file of ratings to return sorted print statements of ratings
+    in original file only"""
+    
     restaurant_ratings = ratings_dictionary(filename)
 
-    # iterates through the list to call the value
+    # Iterate through the list to sort and write original ratings.
     for restaurant, rating in sorted(restaurant_ratings.items()):
         print(f'{restaurant} is rated at {rating}.')
 
-#create our print statement 
-sorted_ratings('scores.txt')
+def add_restaurant(filename):
+    """User adds a restaurant to return sorted print statements of ratings
+    including original file and user input.
+    """
+
+    restaurant_ratings = ratings_dictionary(filename)
+
+    # print please add a rating 
+    print("Please add a rating for the restaurant!")
+
+    # Ask the user for the restaurant and rating
+    restaurant = input("Enter a restaurant's name to rate: ")
+    rating = int(input("Enter a rating for the restaurant (1-10): "))
+
+    # print sorted_ratings including the new rating
+    restaurant_ratings[restaurant] = rating
+
+    # Iterate through the list to sort and write all ratings.
+    for restaurant, rating in sorted(restaurant_ratings.items()):
+        print(f'{restaurant} is rated at {rating}.')
+
+add_restaurant('scores.txt')
 
