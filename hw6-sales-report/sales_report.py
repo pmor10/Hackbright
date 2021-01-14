4aef0e1a25b8330ea5f6d7273a2164d14a9fc574
pmor10 <paresamorton@gmail.com>
2021-01-13 22:22:14 -0800

    Improved the code

--- hw6-sales-report/sales_report.py
+++ hw6-sales-report/sales_report.py
@@ -1,31 +1,44 @@
 """Generate sales report showing total melons each salesperson sold."""
 
+def get_melons_sold_by_salesperson(file_path):
 
-salespeople = [] # create an emply sales people list
-melons_sold = [] # create an empty list for melons sold
+    """Return a dictionary of saleperson name and melons sold
+    
+    Arguments: 
+        file_path (str) - the path to sales report log file
 
-# open the file
-f = open('sales-report.txt')
-# iterating through each line in the file
-for line in f:
-    # remove all white spaces
-    line = line.rstrip()
-    # create a list of data
-    entries = line.split('|')
-    # get the name of the sales person 
-    salesperson = entries[0]
-    # get the number of melons that has been sold by the sales person
-    melons = int(entries[2])
-    # check if salesperson is in the salespeople list
-    if salesperson in salespeople:
-        # find the position of the salesperson in the list
-        position = salespeople.index(salesperson)
-        # use the position to index into melons
-        melons_sold[position] += melons
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+    Return:
+        mels_by_sales (dict)
+    
+    """
 
-# loop over salespeople and use it to index into salespeople and melons sold
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+mels_by_sales = {}
+
+    with open(log_file_path) as f:
+        for line in f:
+            line = line.rstrip()
+
+            # Create a list of data and unpack its values
+            salesperson_name, total_dollars, melons_sold = line.split('|')
+
+            # Set or increment the salesperson's total melons sold
+            if salesperson_name in mels_by_sales:
+                mels_by_sales[salesperson_name] += int(melons_sold)
+            else:
+                mels_by_sales[salesperson_name] = int(melons_sold)
+
+    return mels_by_sales
+
+
+def print_sales_report(melons_sold_by_salesperson):
+    """Print a report of salespeople and the total no. of melons they've sold.
+
+    Arguments:
+        melons_sold_by_salesperson (dict) - {salesperson_name: melons_sold}
+    """
+
+    for salesperson_name, melons_sold in melons_sold_by_salesperson.items():
+        print(f'{salesperson_name} sold {melons_sold} melons')
+
+
+print_sales_report(get_melons_sold_by_salesperson('sales-report.txt'))
